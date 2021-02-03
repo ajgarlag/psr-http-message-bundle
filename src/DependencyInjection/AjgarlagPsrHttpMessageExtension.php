3553f2d6d390a8cc0d6b1fbeec078c7741d4884c
Antonio J. García Lagar <aj@garcialagar.es>
2021-02-03 13:42:47 +0100

    Define autowiring aliases for PSR-17 conditionally

--- src/DependencyInjection/AjgarlagPsrHttpMessageExtension.php
+++ src/DependencyInjection/AjgarlagPsrHttpMessageExtension.php
@@ -12,12 +12,18 @@
 
 namespace Ajgarlag\Bundle\PsrHttpMessageBundle\DependencyInjection;
 
+use Nyholm\Psr7\Factory\Psr17Factory;
+use Psr\Http\Message\ResponseFactoryInterface;
+use Psr\Http\Message\ServerRequestFactoryInterface;
+use Psr\Http\Message\StreamFactoryInterface;
+use Psr\Http\Message\UploadedFileFactoryInterface;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-class AjgarlagPsrHttpMessageExtension extends Extension
+class AjgarlagPsrHttpMessageExtension extends Extension implements CompilerPassInterface
 {
     public function load(array $configs, ContainerBuilder $container)
     {
@@ -30,6 +36,30 @@ class AjgarlagPsrHttpMessageExtension extends Extension
 
         if ($config['alias_sensio_framework_extra_services']['enabled']) {
             $loader->load('alias_sensio_framework_extra.xml');
+        }
+    }
+
+    public function process(ContainerBuilder $container)
+    {
+        $requiredInterfaces = [
+            ResponseFactoryInterface::class,
+            ServerRequestFactoryInterface::class,
+            StreamFactoryInterface::class,
+            UploadedFileFactoryInterface::class,
+        ];
+
+        $nyholmPsr17Id = 'nyholm.psr7.psr17_factory';
+
+        if (class_exists(Psr17Factory::class) && !$container->has($nyholmPsr17Id)) {
+            $container->register($nyholmPsr17Id, Psr17Factory::class);
+        }
+
+        if ($container->has($nyholmPsr17Id)) {
+            foreach ($requiredInterfaces as $requiredInterface) {
+                if (!$container->has($requiredInterface)) {
+                    $container->setAlias($requiredInterface, $nyholmPsr17Id);
+                }
+            }
         }
     }
 }
