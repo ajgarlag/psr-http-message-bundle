5599891d7febc9b3b12904e18e33932a6506e250
Antonio J. García Lagar <aj@garcialagar.es>
2021-02-03 13:58:52 +0100

    Test code with github actions

--- tests/Fixtures/ActionArgumentsBundle/Controller/ActionArgumentsController.php
+++ tests/Fixtures/ActionArgumentsBundle/Controller/ActionArgumentsController.php
@@ -16,24 +16,14 @@ use Psr\Http\Message\MessageInterface;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * @Route("/action-arguments")
- */
 class ActionArgumentsController
 {
-    /**
-     * @Route("/invoke/")
-     */
     public function __invoke(RequestInterface $request, MessageInterface $message, ServerRequestInterface $serverRequest)
     {
         return new Response('<html><body>ok</body></html>');
     }
 
-    /**
-     * @Route("/normal/")
-     */
     public function normalAction(RequestInterface $request, MessageInterface $message, ServerRequestInterface $serverRequest)
     {
         return new Response('<html><body>ok</body></html>');
